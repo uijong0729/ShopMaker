2685d4843c269eb9f8649c3aa503cf592bc265bc
Unknown <krocha0729@gmail.com>
2018-03-30 14:32:48 +0900

    QnA작업중 (QuestionMapper날아간 버전)
    Qna 컨트롤러, 맵퍼(인터페이스). qnajsp 날아간거 복구했는데 불확실함

--- src/main/java/global/sesoc/bigstar/ABoard/AQnAController.java
+++ src/main/java/global/sesoc/bigstar/ABoard/AQnAController.java
@@ -1,17 +1,85 @@
 package global.sesoc.bigstar.ABoard;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import global.sesoc.bigstar.dao.AmemberDAO;
+import global.sesoc.bigstar.dao.AquestionDAO;
+import global.sesoc.bigstar.vo.Aquestion;
 
 @Controller
 public class AQnAController {
-
+	
+	@Autowired
+	AquestionDAO AQdao;
+	
+	@Autowired
+	AmemberDAO AMdao;
+	
+	
 		@RequestMapping(value = "AqnaPage", method = RequestMethod.GET)
-		public String AqnaPage() {
+		public String AqnaPage(Model model) {
+			
+			List<Aquestion> qnaList;
+			qnaList = AQdao.selectAllQuestion();
+			System.out.println(qnaList);
+			
+			for (Aquestion aquestion : qnaList) {
+				String membercode = aquestion.getMembercode();
+				String name = AMdao.selectAcustomerName(membercode);
+				HashMap<String, Object> map = new HashMap<String, Object>(); 
+				
+				map.put("aquestion", aquestion);
+				//map.put("name");
+			}
+			
+			model.addAttribute("qnaList", qnaList);
+			
+			
 			
 			return "Aviews/Aboard/AqnaBoard";
 		}
+		
+		@RequestMapping(value = "AquestionForm", method = RequestMethod.GET)
+		public String AquestionForm() {
+			
+			//System.out.println(AQdao.selectaquestion(0));
+			
+			return "Aviews/Aboard/AquestionForm";
+		}
+		
+		@ResponseBody
+		@RequestMapping(value = "writeQna", method = RequestMethod.POST)
+		public String writeQna(String membercode, String qtitle, String qcontent, String qvisible) {
+			
+			/*System.out.println("writeQna출력사항");
+			System.out.println(membercode);
+			System.out.println(qtitle);
+			System.out.println(qcontent);
+			System.out.println(qvisible);*/
+			
+			Aquestion aq = new Aquestion();
+			
+			aq.setMembercode(membercode);
+			aq.setQtitle(qtitle);
+			aq.setQcontent(qcontent);
+			aq.setQvisible(qvisible);
+			
+			System.out.println("추가 된 문의 : " + AQdao.insertAQuestion(aq));
+			
+			return "confirm";
+		}
+		
 	
 
 	}
