14083746dfbb2ed60e7408581cfd975793f3f8cd
KJH <yo2669@gmail.com>
2018-04-18 14:33:34 +0900

    (feat)Amypage 추가 및 헤더 수정
    (feat)Amypage 추가 및 헤더 수정

--- src/main/java/global/sesoc/bigstar/Amember/AmemberController.java
+++ src/main/java/global/sesoc/bigstar/Amember/AmemberController.java
@@ -21,126 +21,103 @@ import global.sesoc.bigstar.vo.Amember;
 
 @Controller
 public class AmemberController {
-	
+
 	@Inject
 	AmemberDAO AMdao;
-	
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String loginPage() {
-		
+
 		return "home";
 	}
-	
-	
+
 	@RequestMapping(value = "AsignupPage", method = RequestMethod.GET)
 	public String signupPage() {
-		
+
 		return "Aviews/Amember/AsignupPage";
-		
+
 	}
-	
+
 	@RequestMapping(value = "Logout", method = RequestMethod.GET)
 	public String Logout(HttpSession session) {
-		
+
 		session.removeAttribute("Amember");
-		
+
 		return "Aviews/Amember/AwellcomePage";
 	}
-	
+
 	@RequestMapping(value = "AsignupReq", method = RequestMethod.GET)
-	 public String AsignupReq(String id, String password, String emailA
-	         , String emailB, String name, String hpnumber, String addressA
-	         , String addressB, String biznumber)  {
-	         
+	public String AsignupReq(String id, String password, String emailA, String emailB, String name, String hpnumber,
+			String addressA, String addressB, String biznumber) {
+
 		Amember member = new Amember();
-		 
-	      member.setId(id);
-	 
-	      member.setPw(password);
-	 
-	         String mailaddress = emailA + "@" + emailB;
-	 
-	      member.setMailaddress(mailaddress);
-	      member.setName(name);
-	      member.setHpnumber(hpnumber);
-	 
-	         String address = addressA + " " + addressB;
-	 
-	      member.setAddress(address);
-	      member.setBiznumber(biznumber);
-	 
-	      System.out.println(member);
-	      System.out.println(AMdao.AsignupCustomer(member));
-		
+
+		member.setId(id);
+
+		member.setPw(password);
+
+		String mailaddress = emailA + "@" + emailB;
+
+		member.setMailaddress(mailaddress);
+		member.setName(name);
+		member.setHpnumber(hpnumber);
+
+		String address = addressA + " " + addressB;
+
+		member.setAddress(address);
+		member.setBiznumber(biznumber);
+
+		System.out.println(member);
+		System.out.println(AMdao.AsignupCustomer(member));
+
 		return "Aviews/Amember/AwellcomePage";
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value="isThereId", method=RequestMethod.POST)
-	public String isThereId(String id)
-	{
+	@RequestMapping(value = "isThereId", method = RequestMethod.POST)
+	public String isThereId(String id) {
 		String result = AMdao.AfindCustomer(id);
-		
-		if(id.equals(result))
-		{
+
+		if (id.equals(result)) {
 			return id;
-		}
-		else
-		{
+		} else {
 			return "";
 		}
 	}
-	
-	 @RequestMapping(value = "AgoLogin", method = RequestMethod.POST)
-	 
-	   public String AgoLogin(String id, String pw, HttpServletResponse response, Model model, HttpSession session) {
-	      HashMap<String, String> map = new HashMap<String, String>();
-	 
-	      map.put("id", id);
-	      map.put("pw", pw);
-	      
-	      Amember am = AMdao.loginAcustomer(map);
-	      String a= am.getPaymentexpirationdate();
-	      a=a.substring(0, 10);
-	      am.setPaymentexpirationdate(a);
-	      if(am == null)
-	      {
-	 
-	         //model.addAttribute("AloginResult", 1); 
-	 
-	         response.setContentType("text/html; charset=UTF-8");
-	 
-	           PrintWriter out;
-	 
-	         try {
-	 
-	            out = response.getWriter();
-	 
-	            out.println("<script>alert('로그인 정보를 확인해주세요.');</script>");
-	 
-	              out.flush();
-	 
-	         } catch (IOException e) {
 
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	         }
-	         return "Aviews/Amember/Login";
-	 
-	      }
-	 
-	      else
-	 
-	      {
-	         session.setAttribute("Amember", am);
-	         //model.addAttribute("AloginResult", 2)
-	         return "Aviews/Amember/AwellcomePage";
-	        
-	      } 
-	   }
-	 
-	
+	@RequestMapping(value = "AgoLogin", method = RequestMethod.POST)
 
+	public String AgoLogin(String id, String pw, HttpServletResponse response, Model model, HttpSession session) {
+	HashMap<String, String> map = new HashMap<String, String>();
+
+		map.put("id", id);
+		map.put("pw", pw);
+
+		Amember am = AMdao.loginAcustomer(map);
+		String a = am.getPaymentexpirationdate();
+		a = a.substring(0, 10);
+		am.setPaymentexpirationdate(a);
+		if (am == null) {
+			// model.addAttribute("AloginResult", 1);
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out;
+			try {
+				out = response.getWriter();
+				out.println("<script>alert('로그인 정보를 확인해주세요.');</script>");
+				out.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return "Aviews/Amember/Login";
+		}else {
+			session.setAttribute("Amember", am);
+			// model.addAttribute("AloginResult", 2)
+			return "home";
+		}
+	}
+	@RequestMapping(value = "Amypage", method = RequestMethod.GET)
+	public String aMyPage() {
+		return "Aviews/Amember/Amypage";
+	}
 }
-
--- src/main/webapp/WEB-INF/views/Bviews/Bmain/Bshopmain.jsp
+++ src/main/webapp/WEB-INF/views/Bviews/Bmain/Bshopmain.jsp
@@ -5,7 +5,13 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	</head>
-	<body>
-		
+	<body style="left: 0px; top: 0px; right: 0px; bottom: 0px;">
+		<header>
+			<%@ include file="../Bpage/BpageHeader.jsp" %>
+		</header>
+	
+		<%-- <footer>
+			<%@ include file="../Bpage/BpageFooter.jsp" %>
+		</footer> --%>
 	</body>
 </html>
